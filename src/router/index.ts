052a85d874e1c542040f26a6aab7635f01457e89
Uche Victor <flexyvic12@gmail.com>
2022-11-05 23:04:53 -0700

    web hash mode router change

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import Products from "../components/Products.vue";
 import Logistics from "../components/Logistics.vue";
@@ -17,7 +17,7 @@ import NotFound from "@/components/404.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(),//import.meta.env.BASE_URL
+  history: createWebHashHistory(),//import.meta.env.BASE_URL
   routes: [
     {
       path: "/",
