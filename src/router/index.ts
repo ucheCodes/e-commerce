6f34434a58a9827f9dcbd5ff08ec8318bfdc048a
Uche Victor <flexyvic12@gmail.com>
2022-11-05 22:55:05 -0700

    router changes commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,7 @@ import NotFound from "@/components/404.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),//import.meta.env.BASE_URL
   routes: [
     {
       path: "/",
